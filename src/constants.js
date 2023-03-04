963025d3257fc98553e812912bbaba2e2bd23fdf
supminn <30731236+supminn@users.noreply.github.com>
2023-03-04 12:41:55 +0530

    version 1, err attr

--- src/constants.js
+++ src/constants.js
@@ -1,32 +1,32 @@
-export const APPWRITE_ENDPOINT = 'https://demo.appwrite.io/v1';
-export const APPWRITE_PROJECT_ID = 'appwrite-meetup';
-export const APPWRITE_DATABASE_ID = 'main';
-export const COLLECTION_ROOMS = 'rooms';
-export const COLLECTION_MESSAGES = 'messages';
-export const TYPE_VIDEO_OFFER = 'video-offer';
-export const TYPE_VIDEO_ANSWER = 'video-answer';
-export const TYPE_ICE_CANDIDATE = 'new-ice-candidate';
-export const TYPE_HANG_UP = 'hang-up';
+export const APPWRITE_ENDPOINT = "https://bangalore-meetup.appwrite.org/v1";
+export const APPWRITE_PROJECT_ID = "6402e74d580ba9dc7e19";
+export const APPWRITE_DATABASE_ID = "main";
+export const COLLECTION_ROOMS = "rooms";
+export const COLLECTION_MESSAGES = "messages";
+export const TYPE_VIDEO_OFFER = "video-offer";
+export const TYPE_VIDEO_ANSWER = "video-answer";
+export const TYPE_ICE_CANDIDATE = "new-ice-candidate";
+export const TYPE_HANG_UP = "hang-up";
 
 export const servers = {
-    iceServers: [
-        {
-            urls: "stun:relay.metered.ca:80",
-        },
-        {
-            urls: "turn:relay.metered.ca:80",
-            username: "bad128070e40b7a8b3670338",
-            credential: "7sFoXt+jTLSL4Wa2",
-        },
-        {
-            urls: "turn:relay.metered.ca:443",
-            username: "bad128070e40b7a8b3670338",
-            credential: "7sFoXt+jTLSL4Wa2",
-        },
-        {
-            urls: "turn:relay.metered.ca:443?transport=tcp",
-            username: "bad128070e40b7a8b3670338",
-            credential: "7sFoXt+jTLSL4Wa2",
-        }
-    ]
-}
+  iceServers: [
+    {
+      urls: "stun:relay.metered.ca:80",
+    },
+    {
+      urls: "turn:relay.metered.ca:80",
+      username: "bad128070e40b7a8b3670338",
+      credential: "7sFoXt+jTLSL4Wa2",
+    },
+    {
+      urls: "turn:relay.metered.ca:443",
+      username: "bad128070e40b7a8b3670338",
+      credential: "7sFoXt+jTLSL4Wa2",
+    },
+    {
+      urls: "turn:relay.metered.ca:443?transport=tcp",
+      username: "bad128070e40b7a8b3670338",
+      credential: "7sFoXt+jTLSL4Wa2",
+    },
+  ],
+};
--- src/service.js
+++ src/service.js
@@ -1,24 +1,100 @@
-import { account, databases } from "./client"
-import { APPWRITE_DATABASE_ID, COLLECTION_MESSAGES, COLLECTION_ROOMS } from "./constants"
-import { createToastError, createToastSuccess } from "./utils"
+import { account, databases } from "./client";
+import {
+  APPWRITE_DATABASE_ID,
+  COLLECTION_MESSAGES,
+  COLLECTION_ROOMS,
+} from "./constants";
+import { createToastError, createToastSuccess } from "./utils";
 
 export const createAccount = async () => {
-}
+  try {
+    await account.createAnonymousSession();
+  } catch (error) {
+    createToastError(error.message);
+  }
+};
 
 export const getAccount = async (silent = true) => {
-}
+  try {
+    let response = await account.get();
+    return response;
+  } catch (error) {
+    if (!silent) {
+      createToastError(error.message);
+    }
+  }
+};
 
 export const createRoom = async (roomId, caller) => {
-}
+  try {
+    let response = await databases.createDocument(
+      APPWRITE_DATABASE_ID,
+      COLLECTION_ROOMS,
+      roomId,
+      {
+        caller,
+      }
+    );
+    return response;
+  } catch (err) {
+    createToastError(err.message);
+  }
+};
 
 export const updateRoom = async (roomId, caller, callee, silent = true) => {
-}
+  try {
+    let res = await databases.updateDocument(
+      APPWRITE_DATABASE_ID,
+      COLLECTION_ROOMS,
+      roomId,
+      {
+        caller,
+        callee,
+      }
+    );
+    return res;
+  } catch (err) {
+    createToastError(err.message);
+  }
+};
 
 export const getMessages = async (queries) => {
-}
+  try {
+    let res = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      COLLECTION_ROOMS,
+      queries
+    );
+    return res;
+  } catch (err) {
+    createToastError(err.message);
+  }
+};
 
 export const getRoom = async (roomId) => {
-}
+  try {
+    let res = await databases.getDocument(
+      APPWRITE_DATABASE_ID,
+      COLLECTION_ROOMS,
+      roomId
+    );
+    return res;
+  } catch (err) {
+    createToastError(err.message);
+  }
+};
 
 export const sendToServer = async (message) => {
+  try {
+    let payload = JSON.stringify(message.payload);
+    let res = await databases.createDocument(
+      APPWRITE_DATABASE_ID,
+      COLLECTION_MESSAGES,
+      "unique()",
+      { ...message, payload }
+    );
+    return res;
+  } catch (err) {
+    createToastError(err.message);
+  }
 };
